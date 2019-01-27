08ec410d4afef03ac8f871633ad5f6dc47630563
Amin Bashiri <amin.bashiri28@gmail.com>
2019-01-27 15:44:15 +0330

    (story/170): Handle 500 error for axios call

--- src/HttpProvider/HttpProvider.ts
+++ src/HttpProvider/HttpProvider.ts
@@ -1,22 +1,30 @@
-import IHttpProvider, {IHttpRequest, IHttpResponse} from "./IHttpProvider";
+import IHttpProvider, { IHttpRequest, IHttpResponse } from "./IHttpProvider";
 import axios from "axios";
+
+const SERVER_ERROR_PATH = "/500";
 
 export default class HttpProvider implements IHttpProvider {
 	send(request: IHttpRequest): Promise<IHttpResponse> {
+		let responseObj: IHttpResponse;
+
+		setTimeout(() => {
+			if (!responseObj) {
+				location.replace(SERVER_ERROR_PATH);
+			}
+		}, 5000);
+
 		return axios({
 			data: request.body,
 			url: request.url,
 			headers: request.header,
 			method: request.method,
 		}).then(response => {
-			const responseObj: IHttpResponse= {
+			responseObj = {
 				statusCode: response.status,
 				body: response.data.data,
 				header: response.headers,
 			};
 			return responseObj;
-
 		});
 	}
-
 }
